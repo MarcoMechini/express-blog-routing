cc85222571176083b8823b9cb634b4bf7c2b001f
Marco Mechini <mechinimarco35@gmail.com>
2024-12-03 15:23:10 +0100

    git init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const post = require("./data");
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.json('Hello');
+});
+
+//nella route bacheca abbiamo come risposta un json che ha come parametri post e la lunghezza
+app.get('/bacheca', (req, res) => {
+    res.json({
+        posts: post.data,
+        length: post.data.length,
+    })
+
+});
+
+app.listen(port, () => {
+    console.log("Server on");
+});
+
